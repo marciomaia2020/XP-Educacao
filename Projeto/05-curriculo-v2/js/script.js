fa0c7d9a0efd9ec7cb69690365b263b38dbd3932
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2023-07-26 15:43:06 -0300

    Add files via upload

---
+++ Projeto/05-curriculo-v2/js/script.js
@@ -0,0 +1,14 @@
+
+console.log("Testando a conexão do java script na console");
+
+
+/*
+console.log("teste");
+
+<script>
+a = 5; b = 6; c = a + b;
+console.log(c);
+</script>
+
+windows.alert("Olá Mundo!");
+*/
